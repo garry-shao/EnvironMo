62c9c605f969bbb2d0d3cf47c60f5cf0a06bfffb
SS <SS@SSS>
2015-11-29 16:38:45 +0800

    Refactor using inner class.

--- EnvironMo/src/org/qmsos/environmo/MainActivity.java
+++ EnvironMo/src/org/qmsos/environmo/MainActivity.java
@@ -24,92 +24,92 @@ import android.widget.TextView;
  * @author EnvironMo
  * 
  */
-public class MainActivity extends Activity implements OnRefreshListener {
-	
+public class MainActivity extends Activity {
+
 	private static CityInfo cityInfo = new CityInfo(0, null, null, 0, 0);
 	private static WeatherInfo currentWeather = new WeatherInfo(null, null);
 	private static LongSparseArray<WeatherInfo> forecastWeather = new LongSparseArray<WeatherInfo>();
 
 	private EnvironRefreshLayout swipeRefresh;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		swipeRefresh = (EnvironRefreshLayout) findViewById(R.id.swipe_refresh);
-		swipeRefresh.setOnRefreshListener(this);
 		ScrollView scrollView = (ScrollView) findViewById(R.id.scroll_view);
 		swipeRefresh.setScrollView(scrollView);
-		
+		swipeRefresh.setOnRefreshListener(new OnRefreshListener() {
+
+			@Override
+			public void onRefresh() {
+				new Handler().postDelayed(new Runnable() {
+
+					@Override
+					public void run() {
+						swipeRefresh.setRefreshing(false);
+
+						updateContent();
+					}
+				}, 500);
+			}
+		});
+
 		updateUI();
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		
+
 		updateUI();
-	}
-	
-	@Override
-	public void onRefresh() {
-		new Handler().postDelayed(new Runnable() {
-
-			@Override
-			public void run() {
-				swipeRefresh.setRefreshing(false);
-
-				updateContent();
-			}
-		}, 500);
 	}
 
 	public void settingCity(View view) {
 		startActivity(new Intent(getApplicationContext(), SelectActivity.class));
 	}
-	
+
 	private void updateContent() {
 		PendingIntent pendingResult = createPendingResult(0, new Intent(), 0);
 		Intent intent = new Intent(getApplicationContext(), MainUpdateService.class);
 		intent.putExtra(MainUpdateService.EXTRA_PENDING_RESULT, pendingResult);
-		
+
 		startService(intent);
 	}
-	
+
 	private void parseCurrentWeather() {
 		try {
-			SharedPreferences prefs = 
-					PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
 			String currentWeatherJSON = prefs.getString(MainUpdateService.CURRENT_WEATHER, "null");
 			JSONObject reader = new JSONObject(currentWeatherJSON);
-			
+
 			int id = reader.getInt("id");
 			String name = reader.getString("name");
-			
+
 			JSONObject sys = reader.getJSONObject("sys");
 			String country = sys.getString("country");
 
 			JSONObject coord = reader.getJSONObject("coord");
 			double longitude = coord.getDouble("lon");
 			double latitude = coord.getDouble("lat");
-	
+
 			cityInfo = new CityInfo(id, name, country, longitude, latitude);
-			
+
 			JSONArray weather = reader.getJSONArray("weather");
 			String weatherMain = weather.getJSONObject(0).getString("main");
 			String weatherDescription = weather.getJSONObject(0).getString("description");
-			
+
 			JSONObject main = reader.getJSONObject("main");
 			int temperature = main.getInt("temp");
 			int pressure = main.getInt("pressure");
 			int humidity = main.getInt("humidity");
-			
+
 			JSONObject wind = reader.getJSONObject("wind");
 			int windSpeed = wind.getInt("speed");
 			int windDirection = wind.getInt("deg");
-			
+
 			currentWeather = new WeatherInfo(weatherMain, weatherDescription);
 			currentWeather.setTemperature(temperature);
 			currentWeather.setPressure(pressure);
@@ -126,33 +126,32 @@ public class MainActivity extends Activity implements OnRefreshListener {
 	private void parseForecastWeather() {
 		try {
 			forecastWeather.clear();
-			
-			SharedPreferences prefs = 
-					PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
 			String forecastWeatherJSON = prefs.getString(MainUpdateService.FORECAST_WEATHER, "null");
 			JSONObject reader = new JSONObject(forecastWeatherJSON);
-	
+
 			JSONArray list = reader.getJSONArray("list");
-			for (int i = 0; i < list.length(); i++) { 
+			for (int i = 0; i < list.length(); i++) {
 				JSONObject forecast = list.getJSONObject(i);
-				
+
 				long date = forecast.getLong("dt");
-				
+
 				JSONArray weather = forecast.getJSONArray("weather");
 				String weatherMain = weather.getJSONObject(0).getString("main");
 				String weatherDescription = weather.getJSONObject(0).getString("description");
-				
+
 				JSONObject temp = forecast.getJSONObject("temp");
 				int temperatureMin = temp.getInt("min");
 				int temperatureMax = temp.getInt("max");
-				
+
 				int pressure = forecast.getInt("pressure");
 				int humidity = forecast.getInt("humidity");
-				
+
 				int windSpeed = forecast.getInt("speed");
 				int windDirection = forecast.getInt("deg");
-				
+
 				WeatherInfo weatherInfo = new WeatherInfo(weatherMain, weatherDescription);
 				weatherInfo.setTemperatureMin(temperatureMin);
 				weatherInfo.setTemperatureMax(temperatureMax);
@@ -160,24 +159,24 @@ public class MainActivity extends Activity implements OnRefreshListener {
 				weatherInfo.setHumidity(humidity);
 				weatherInfo.setWindSpeed(windSpeed);
 				weatherInfo.setWindDirection(windDirection);
-				
+
 				forecastWeather.put(date, weatherInfo);
 			}
-			
+
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void updateUI() {
 		parseCurrentWeather();
 		parseForecastWeather();
-		
+
 		updateCityUI();
 		updateCurrentUI();
 		updateForecastUI();
 	}
-	
+
 	private void updateCityUI() {
 		TextView textView = (TextView) findViewById(R.id.city_name);
 		if (cityInfo.getName() != null) {
@@ -191,28 +190,26 @@ public class MainActivity extends Activity implements OnRefreshListener {
 
 		textView = (TextView) findViewById(R.id.city_coordinates);
 		double latitude = cityInfo.getLatitude();
-		String latString = latitude > 0 ? 
-				String.valueOf(latitude) + "\u00B0N" : String.valueOf(latitude) + "\u00B0S";
+		String latString = latitude > 0 ? String.valueOf(latitude) + "\u00B0N" : String.valueOf(latitude) + "\u00B0S";
 		double longitude = cityInfo.getLongitude();
-		String lonString = longitude > 0 ? 
-				String.valueOf(longitude) + "\u00B0E" : String.valueOf(longitude) + "\u00B0W";
+		String lonString = longitude > 0 ? String.valueOf(longitude) + "\u00B0E"
+				: String.valueOf(longitude) + "\u00B0W";
 		textView.setText(latString + "/" + lonString);
-		
+
 	}
-	
+
 	private void updateCurrentUI() {
-		
+
 		TextView textView = (TextView) findViewById(R.id.current_temperature);
-		textView.setText(
-				String.valueOf(currentWeather.getTemperature()) + "\u00b0" + "C");
+		textView.setText(String.valueOf(currentWeather.getTemperature()) + "\u00b0" + "C");
 
 		textView = (TextView) findViewById(R.id.current_main);
 		if (currentWeather.getWeatherMain() != null) {
 			textView.setText(currentWeather.getWeatherMain());
 		}
-	
+
 	}
-	
+
 	private void updateForecastUI() {
 		for (int i = 0; i < forecastWeather.size(); i++) {
 			long date = forecastWeather.keyAt(i);
@@ -222,18 +219,17 @@ public class MainActivity extends Activity implements OnRefreshListener {
 				TextView textView = (TextView) findViewById(
 						getResources().getIdentifier("forecast_date_" + i, "id", getPackageName()));
 				textView.setText(DateFormat.format("MM-dd", date * 1000).toString());
-				
+
 				textView = (TextView) findViewById(
 						getResources().getIdentifier("forecast_temperature_" + i, "id", getPackageName()));
-				textView.setText(
-						String.valueOf(forecast.getTemperatureMin()) + "\u00B0" + "/" + 
-						String.valueOf(forecast.getTemperatureMax()) + "\u00B0");
-				
+				textView.setText(String.valueOf(forecast.getTemperatureMin()) + "\u00B0" + "/"
+						+ String.valueOf(forecast.getTemperatureMax()) + "\u00B0");
+
 				textView = (TextView) findViewById(
 						getResources().getIdentifier("forecast_main_" + i, "id", getPackageName()));
 				textView.setText(forecast.getWeatherMain());
 			}
 		}
 	}
-	
+
 }
