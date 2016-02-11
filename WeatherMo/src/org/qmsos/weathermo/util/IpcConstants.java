26127789b474d7108e22c4bc613eb079285caabc
SS <SS@SSS>
2016-02-11 12:28:30 +0800

    Fix up: add package name to intent's extra complying android code style.

--- WeatherMo/src/org/qmsos/weathermo/util/IpcConstants.java
+++ WeatherMo/src/org/qmsos/weathermo/util/IpcConstants.java
@@ -6,20 +6,20 @@ public class IpcConstants {
 	
 	public static final String ACTION_REFRESH_WEATHER = BASE_PACKAGE_NAME + "ACTION_REFRESH_WEATHER";
 	
-	public static final String ACTION_DELETE_CITY = BASE_PACKAGE_NAME +"ACTION_DELETE_CITY";
-	public static final String EXTRA_CITY_ID = "EXTRA_CITY_ID";
+	public static final String ACTION_DELETE_CITY = BASE_PACKAGE_NAME + "ACTION_DELETE_CITY";
+	public static final String EXTRA_CITY_ID = BASE_PACKAGE_NAME + "EXTRA_CITY_ID";
 	
 	public static final String ACTION_QUERY_CITY = BASE_PACKAGE_NAME + "ACTION_QUERY_CITY";
-	public static final String EXTRA_CITY_NAME = "EXTRA_CITY_NAME";
+	public static final String EXTRA_CITY_NAME = BASE_PACKAGE_NAME + "EXTRA_CITY_NAME";
 
 	public static final String ACTION_QUERY_EXECUTED = BASE_PACKAGE_NAME + "ACTION_QUERY_EXECUTED";
-	public static final String EXTRA_QUERY_EXECUTED = "EXTRA_QUERY_EXECUTED";
-	public static final String EXTRA_QUERY_RESULT = "EXTRA_QUERY_RESULT";
+	public static final String EXTRA_QUERY_EXECUTED = BASE_PACKAGE_NAME + "EXTRA_QUERY_EXECUTED";
+	public static final String EXTRA_QUERY_RESULT = BASE_PACKAGE_NAME + "EXTRA_QUERY_RESULT";
 
 	public static final String ACTION_ADD_CITY = BASE_PACKAGE_NAME + "ACTION_ADD_CITY";
 	public static final String EXTRA_ADD_CITY = BASE_PACKAGE_NAME + "EXTRA_ADD_CITY";
 
 	public static final String ACTION_ADD_EXECUTED = BASE_PACKAGE_NAME + "ACTION_ADD_EXECUTED";
-	public static final String EXTRA_ADD_EXECUTED = "EXTRA_ADD_EXECUTED";
+	public static final String EXTRA_ADD_EXECUTED = BASE_PACKAGE_NAME + "EXTRA_ADD_EXECUTED";
 
 }
