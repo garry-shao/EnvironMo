d7749cb26fec147cf00e1cbfc30b7ac5546c5853
SS <SS@SSS>
2016-03-18 21:19:07 +0800

    Fix up: RecyclerView can handle ViewState.
    Handle InstanceState should follow this principal: 
    	Do not mix up DIFFERENT TYPE of elements.
    
    View should handle ViewState themselves, Fragment should handle states
    that other than ViewState, Do not over step. Should follow android
    design principle of different layers of abstraction.

--- WeatherMo/src/org/qmsos/weathermo/fragment/CityList.java
+++ WeatherMo/src/org/qmsos/weathermo/fragment/CityList.java
@@ -6,25 +6,20 @@ import org.qmsos.weathermo.widget.CityListRecyclerViewAdapter;
 
 import android.database.Cursor;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.LayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class CityList extends Fragment implements LoaderCallbacks<Cursor> {
 
-	private static final String KEY_RECYCLER_VIEW_STATE = "KEY_RECYCLER_VIEW_STATE";
-
 	private CityListRecyclerViewAdapter mCityListAdapter;
-	private RecyclerView mRecyclerView;
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_city_list, container, false);
@@ -38,9 +33,9 @@ public class CityList extends Fragment implements LoaderCallbacks<Cursor> {
 		
 		mCityListAdapter = new CityListRecyclerViewAdapter(getContext(), null);
 		
-		mRecyclerView = (RecyclerView) view.findViewById(R.id.city_list);
-		mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-		mRecyclerView.setAdapter(mCityListAdapter);
+		RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.city_list);
+		recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+		recyclerView.setAdapter(mCityListAdapter);
 	}
 
 	@Override
@@ -55,29 +50,6 @@ public class CityList extends Fragment implements LoaderCallbacks<Cursor> {
 		getLoaderManager().destroyLoader(0);
 		
 		super.onDestroyView();
-	}
-
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		
-		LayoutManager layoutManager = mRecyclerView.getLayoutManager();
-		if (layoutManager instanceof LinearLayoutManager) {
-			outState.putParcelable(KEY_RECYCLER_VIEW_STATE, layoutManager.onSaveInstanceState());
-		}
-	}
-
-	@Override
-	public void onViewStateRestored(Bundle savedInstanceState) {
-		super.onViewStateRestored(savedInstanceState);
-		
-		if (savedInstanceState != null) {
-			Parcelable savedRecyclerViewState = savedInstanceState.getParcelable(KEY_RECYCLER_VIEW_STATE);
-			LayoutManager layoutManager = mRecyclerView.getLayoutManager();
-			if (layoutManager instanceof LinearLayoutManager) {
-				layoutManager.onRestoreInstanceState(savedRecyclerViewState);
-			}
-		}
 	}
 
 	@Override
